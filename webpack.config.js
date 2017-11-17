e1f6c728afb9fd7fc4afb0eaee1d825fee7a815a
Raphael Lupchinski <27raphalupi@gmail.com>
2017-11-16 22:04:55 -0200

    setting up gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     entry: './src/index.js',
 
     output: {
-        path: path.resolve('dist'),
+        path: path.resolve('docs'),
         filename: 'index_bundle.js',
         publicPath: '/'
     },
